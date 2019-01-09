2d570a166d4aa0eb1923ba13d6dea42db77a60ce
washworth01 <45197980+washworth01@users.noreply.github.com>
2019-01-09 11:44:35 +0000

    Update jenkinsInstall.sh

--- jenkinsInstall.sh
+++ jenkinsInstall.sh
@@ -1,6 +1,7 @@
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 echo "deb https://pkg.jenkins.io/debian binary/" | sudo tee -a /etc/apt/sources.list
 sudo apt install openjdk-8-jre-headless
+sud apt install openjdk-8-jdk
 sudo apt-get update
 sudo apt-get install jenkins
 sudo apt install curl
